b946769ec61afdf186405d573338e123a12c30d2
gitpratap <mbhanupratap720@gmail.com>
2024-01-26 22:21:08 -0500

    Added Successfully

---
+++ src/Static_Classs/Static_class.java
@@ -0,0 +1,28 @@
+package Static_Classs;
+
+public class Static_class {
+	
+	
+
+		String name;
+		String color;
+		double price;
+		static int wheels = 4;
+		
+		public static void start()
+		{
+			System.out.println("Start Car");
+		}
+		
+		public void invokeSportDrive()
+		{
+			System.out.println("Sport Drive Mode Enabled");
+		}
+		
+		public static void stop()
+		{
+			System.out.println("Stop the Car");
+		}	
+	}
+
+
---
+++ src/Static_Classs_2/Static_Classs_2.java
@@ -0,0 +1,19 @@
+package Static_Classs_2;
+
+import Static_Classs.Static_class;
+
+public class Static_Classs_2 {
+
+	public static void main(String[] args) {
+		{
+			Static_class.start();
+			
+			Static_class mycar = new Static_class();
+			mycar.invokeSportDrive();
+			
+			Static_class.stop();		
+		}
+
+	}
+
+}
